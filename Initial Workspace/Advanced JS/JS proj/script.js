c8de03d8dfa3d82e471ffa95a85aac2d0b3d79cc
beni2408 <jascarbenish@gmail.com>
2025-07-21 20:15:18 +0530

    final push today

--- Initial Workspace/Advanced JS/JS proj/script.js
+++ Initial Workspace/Advanced JS/JS proj/script.js
@@ -1,5 +1,7 @@
 let formContainer = document.createElement("div");
 let mainContainer = document.getElementById("main");
+let listContainer = document.createElement("div");
+// listContainer.setAttribute("class");
 formContainer.setAttribute("id", "form-box");
 formContainer.innerHTML += `
 <form class= " shadow-xl w-[700px] h-[400px]  flex flex-col items-center   justify-center gap-3">
@@ -10,7 +12,7 @@ formContainer.innerHTML += `
 <button  id="save-btn" class =  " righteous-regular bg-black p-2 w-36 rounded-lg text-white font-semibold">Save</button>
 </form>
 `;
-mainContainer.append(formContainer);
+mainContainer.append(formContainer, listContainer);
 
 let nameInput = document.getElementById("name-input");
 let batchInput = document.getElementById("batch-input");
@@ -37,3 +39,21 @@ formContainer.addEventListener("submit", (e) => {
     removeInputField();
   }
 });
+const dummyObj = {
+  student_name: "Jascar",
+  student_batch: "B29",
+  student_course: "Full Stack",
+};
+function createStudentCard(studentObj) {
+  listContainer.innerHTML += `
+    <div class="  flex justify-start p-4 flex-wrap items-center">
+    <div class = "bg-slate-200 p-4 rounded-lg w-[150px] h-[200px] flex flex-col shadow-lg"> 
+    <div class="text-2xl text-center font-bold righteous-regular">${studentObj.student_name}</div>
+    <div class = "opacity-75 font-semibold ">${studentObj.student_batch}</div>
+    <div class ="opacity-75 font-semibold " >${studentObj.student_course}</div>
+    </div>
+    </div>
+  `;
+}
+
+createStudentCard(dummyObj);
