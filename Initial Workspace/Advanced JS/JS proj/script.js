d2479607584c7ca28f9e8e768f641fe67f3215e3
beni2408 <jascarbenish@gmail.com>
2025-07-22 20:02:14 +0530

    worked on the application

--- Initial Workspace/Advanced JS/JS proj/script.js
+++ Initial Workspace/Advanced JS/JS proj/script.js
@@ -1,15 +1,31 @@
 let formContainer = document.createElement("div");
 let mainContainer = document.getElementById("main");
 let listContainer = document.createElement("div");
-// listContainer.setAttribute("class");
+let searchContainer = document.createElement("div");
+listContainer.setAttribute(
+  "class",
+  "flex justify-start items-center p-4 flex-wrap gap-2"
+);
 formContainer.setAttribute("id", "form-box");
+formContainer.setAttribute(
+  "class",
+  "flex items-center gap-2 flex-col border-1"
+);
 formContainer.innerHTML += `
 <form class= " shadow-xl w-[700px] h-[400px]  flex flex-col items-center   justify-center gap-3">
 <h1 class="text-3xl font-bold righteous-regular"> Student Portal form</h1>
 <input required type="text" id="name-input" class="  focus:outline-none p-4  bg-slate-200 rounded-md border-1  w-[90%]" placeholder = "Enter your name">
 <input required type="text" id="batch-input"  class="  focus:outline-none p-4 bg-slate-200 rounded-md  border-1 w-[90%]"placeholder = "Enter your Batch no.">
 <input required type="text" id="course-input" class="  focus:outline-none p-4 bg-slate-200 rounded-md  border-1 w-[90%]" placeholder = "Enter course name">
-<button  id="save-btn" class =  " righteous-regular bg-black p-2 w-36 rounded-lg text-white font-semibold">Save</button>
+<button  type = "submit"id="save-btn" class =  " righteous-regular bg-black p-2 w-36 rounded-lg text-white font-semibold">Save</button>
+</form>
+`;
+
+searchContainer.addEventListener();
+searchContainer.innerHTML = `
+<form>
+  <input  type="text" id="name-input" class="focus:outline-none p-4  bg-slate-200 rounded-md border-1  w-[70%]" placeholder = "Search">
+  <button  class =  " cursor-pointer righteous-regular bg-black p-2 w-36 rounded-lg text-white font-semibold>Search</button>
 </form>
 `;
 mainContainer.append(formContainer, listContainer);
@@ -25,6 +41,14 @@ function removeInputField() {
   batchInput.value = "";
   courseInput.value = "";
 }
+
+let student_list = [
+  {
+    student_name: "Jascar",
+    student_batch: "29",
+    student_course: "MERN",
+  },
+];
 
 formContainer.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -56,4 +80,14 @@ function createStudentCard(studentObj) {
   `;
 }
 
-createStudentCard(dummyObj);
+// createStudentCard(dummyObj);
+// createStudentCard(newStudentObject);
+
+function renderAllStudentsCard(studenList) {
+  studenList.map((stud_obj, index, accARR) => {
+    createStudentCard(stud_obj);
+  });
+}
+if (student_list.length > 0) {
+  renderAllStudentsCard(student_list);
+}
