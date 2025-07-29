77bfddd96cab55b96f855123a6a20352cebe1f9c
beni2408 <jascarbenish@gmail.com>
2025-07-29 19:43:49 +0530

    fetched API using the async method also

--- Initial Workspace/Advanced JS/JS proj/event/events.js
+++ Initial Workspace/Advanced JS/JS proj/event/events.js
@@ -1,6 +1,51 @@
-//
-//fetch
+// //
+// //fetch
 
-fetch("https://restcountries.com/v3.1/independent?status=true", {
-  method: "GET",
-}).then((res) => console.log(res).catch((err) => console.log(err)));
+// fetch("https://restcountries.com/v3.1/independent?status=true", {
+//   method: "GET",
+// }).then((res) => console.log(res).catch((err) => console.log(err)));
+
+let mainContainer = document.getElementById("main");
+let cardList = document.createElement("div");
+cardList.setAttribute("class", "cardList");
+
+function createNewCard(value) {
+  cardList.innerHTML += `
+    <div class="card">
+    <img class = "card-image" src=${value.flags.png} alt=${"image"}/>
+    <div class = "card-name">${value.name.common}<div>
+    </div>
+    `;
+}
+
+function renderAllcard(countryArr) {
+  console.log(countryArr);
+  countryArr.forEach((value) => {
+    createNewCard(value);
+  });
+}
+// fetch("https://restcountries.com/v3.1/independent?status=true", {
+//   method: "GET",
+// })
+//   .then((response) => response.json())
+//   .then((data) => renderAllcard(data))
+//   .catch((err) => console.log(err));
+
+//async method
+
+async function getcountryData() {
+  try {
+    let ftdata = await fetch(
+      "https://restcountries.com/v3.1/independent?status=true",
+      {
+        method: "GET",
+      }
+    );
+    let gotdt = await ftdata.json();
+    let render = renderAllcard(gotdt);
+  } catch (err) {
+    console.log(err);
+  }
+}
+mainContainer.append(cardList);
+getcountryData();
