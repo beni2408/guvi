644cf308a74f8427f5dbb1afc3b10648e221ab81
beni2408 <jascarbenish@gmail.com>
2025-07-30 19:20:12 +0530

    completed the Advanced Js concepts

--- Initial Workspace/Advanced JS/JS proj/event/events.js
+++ Initial Workspace/Advanced JS/JS proj/event/events.js
@@ -5,6 +5,7 @@
 //   method: "GET",
 // }).then((res) => console.log(res).catch((err) => console.log(err)));
 
+// restapi
 let mainContainer = document.getElementById("main");
 let cardList = document.createElement("div");
 cardList.setAttribute("class", "cardList");
@@ -19,7 +20,7 @@ function createNewCard(value) {
 }
 
 function renderAllcard(countryArr) {
-  console.log(countryArr);
+  // console.log(countryArr);
   countryArr.forEach((value) => {
     createNewCard(value);
   });
@@ -49,3 +50,54 @@ async function getcountryData() {
 }
 mainContainer.append(cardList);
 getcountryData();
+
+//
+
+//REST
+let someArr = ["value1", "value2", "value3"];
+console.log(...someArr);
+
+let someObj = {
+  name: "value1",
+  key2: "value2",
+};
+
+// console.log(...someObj);
+
+function someFunc(param1, param2, param3) {
+  console.log(param1);
+  console.log(param2);
+  console.log(param3);
+}
+function someRest(param1, ...param2) {
+  console.log(param1);
+  console.log(param2);
+}
+
+someFunc(...someArr); //(value1, value2, value3)
+someRest("value1", "value2", "value3", "value4"); //(value1, value2, value3)
+
+const arr1 = ["jascar", "Rohan", "Shiny"];
+const arr2 = ["Sam", "rohan", "Vino"];
+const movedarr = [...arr1, ...arr2];
+console.log(movedarr);
+
+const newObj = {
+  name: "newObj",
+  ...arr1,
+};
+
+console.log(newObj);
+
+// array destructuring
+let [idx1, idx2, idx3] = arr1;
+console.log(idx1);
+console.log(idx2);
+console.log(idx3);
+// we can give empty for 2nd value if we only need the 1st and 3rd
+
+const { name } = someObj;
+console.log(name);
+
+const { 2: key2val } = newObj;
+console.log(key2val);
