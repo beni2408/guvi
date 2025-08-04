749cb06b3772cac31bca5c0b8865a45afedc7a79
beni2408 <jascarbenish@gmail.com>
2025-08-04 19:32:24 +0530

    worked on some examples

--- Initial Workspace/react-app/src/App.jsx
+++ Initial Workspace/react-app/src/App.jsx
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { createElement } from "react";
 import "./App.css";
-
 function App() {
   return (
     <>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Exampleelement></Exampleelement>
+      <DivButton></DivButton>
+      <Propsexample
+        className={"div"}
+        element={"DivButton"}
+        component={Exampleelement()}
+      ></Propsexample>
     </>
   );
 }
 
 export default App;
 
-function Button() {
-  return <button className="btn">Save</button>;
+function Exampleelement() {
+  return (
+    <div className="div">
+      <label htmlFor="input">Enter name</label>
+      <input className="input" type="text" name="name" />
+      <button className="btn">Button</button>
+    </div>
+  );
+}
+
+function MyButton() {
+  return createElement(
+    "button",
+    { className: "mybtn1", id: "my-btnid" },
+    "Save"
+  );
+}
+
+function DivButton() {
+  return createElement(
+    "div",
+    {
+      className: "div",
+    },
+    createElement("button", { className: "btn" }, "Save")
+  );
+}
+
+function Propsexample(props) {
+  return (
+    <div className={props.className}>
+      {props.element}
+      {props.component}
+    </div>
+  );
 }
